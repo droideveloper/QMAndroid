cf128b3bd0a5f5a3ff76bef2c3aed12c47367123
droideveloper <fatih.sen.boun@gmail.com>
2016-06-12 19:40:01 +0300

    first

---
+++ app/src/main/java/org/fs/qm/ScrollingActivity.java
@@ -0,0 +1,16 @@
+package org.fs.qm;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+
+public class ScrollingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_scrolling);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+}
---
+++ app/src/main/java/org/fs/qm/holders/ILabelTypeHolder.java
@@ -0,0 +1,7 @@
+package org.fs.qm.holders;
+
+/**
+ * Created by Fatih on 12/06/16.
+ * as org.fs.qm.holders.ILabelHolder
+ */
+public interface ILabelTypeHolder { /** TYPE_DEFINITION  **/ }
